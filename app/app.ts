cace0786d2e98edc0dea9261adc3dd53a64f0fd3
JuMaLaB <julien.malaboeuf@gmail.com>
2021-12-20 15:14:28 +0100

    03 - log a user and use strictNullChecks compiler option

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,14 @@
-"use strict";
 function startGame() {
-  // starting new game
-  var messagesElement = document.getElementById('messages');
+  // starting a new game
+  let playerName: string = 'Audrey';
+  logPlayer(playerName);
+
+  let messagesElement = document.getElementById('messages');
   messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
 }
+
+function logPlayer(name) {
+  console.log(`New game starting for player: ${name}`);
+}
+
 document.getElementById('startGame')!.addEventListener('click', startGame);
